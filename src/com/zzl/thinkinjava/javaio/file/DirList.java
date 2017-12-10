7a18faccb9033e1d4432fcbf7811d9423252f911
zhanyi <847552488@qq.com>
2017-12-10 23:06:21 +0800

    test

--- src/com/zzl/thinkinjava/javaio/file/DirList.java
+++ src/com/zzl/thinkinjava/javaio/file/DirList.java
@@ -7,17 +7,30 @@ import java.util.regex.Pattern;
 
 public class DirList {
 
-    public static void main(final String[] args) {
 
+
+    public static void main(final String[] args) {
+        DirList dirList = new DirList("./src/com/zzl/thinkinjava/javaio/file/test");
+//        System.out.println(dirList.);
+        dirList.fileFilter("[a-zA-Z0-9]*.c");
     }
 
-    public static void DirList1(final String[] args) {
-        File path = new File(".");
+
+    private String filePath;
+
+    public DirList(String filePath) {
+        this.filePath = filePath;
+    }
+
+
+    public void fileFilter(String filterRegex) {
+        File path = new File(filePath);
+        System.out.println("absolutely path:" + path.getAbsolutePath());
         String[] list;
-        if (args.length == 0) {
-            list = path.list();
+        if (filterRegex != null && !filterRegex.equals("")) {
+            list = path.list(new DirNameFilter(filterRegex));
         } else {
-            list = path.list(new DirNameFilter(""));
+            list = path.list();
         }
 
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
